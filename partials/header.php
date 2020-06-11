dd3c70c76e3c11027e9a21b7072809bd9a62ba21
Alan Pereira <alanpfabricio@gmail.com>
2020-06-10 22:35:34 -0300

    Create header.php

---
+++ partials/header.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Faculdade Pólis Civitas</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="style/css/bootstrap.min.css">
+
+    <!-- bootstrap-select -->
+    <link rel="stylesheet" href="style/bootstrap-select/css/bootstrap-select.min.css">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="style/stylesheet.css">
+
+    <!-- Fontes -->
+    <link href="https://fonts.googleapis.com/css?family=Oswald&display=swap" rel="stylesheet">
+</head>
